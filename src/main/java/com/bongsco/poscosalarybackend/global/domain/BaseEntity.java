52189107b104f7ad4dfc55a3cdafd5735c82e198
lej8924 <eunjae8924@naver.com>
2025-03-07 14:33:57 +0900

    🎨 Improve: swagger 기능 추가

--- src/main/java/com/bongsco/poscosalarybackend/global/domain/BaseEntity.java
+++
@@ -1,6 +0,0 @@
-package com.bongsco.poscosalarybackend.global.domain;
-
-public class BaseEntity {
-    public static class Status {
-    }
-}
--- src/main/java/com/bongsco/poscosalarybackend/user/domain/Rank.java
+++ src/main/java/com/bongsco/poscosalarybackend/user/domain/Rank.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table(name = "rank")
+@Table(name = "`rank`")
 @Data
 public class Rank {
     @Id
