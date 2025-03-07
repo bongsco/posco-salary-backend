52957353b763471fe7b725b7f8b0b715a2b457d1
lej8924 <eunjae8924@naver.com>
2025-03-07 16:56:54 +0900

    🐛 Fix: code formatter

--- src/main/java/com/bongsco/poscosalarybackend/user/domain/Grade.java
+++ src/main/java/com/bongsco/poscosalarybackend/user/domain/Grade.java
@@ -1,9 +1,14 @@
 package com.bongsco.poscosalarybackend.user.domain;
 
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "grade")
