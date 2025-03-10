66d3b8dc98500dca4c99669c4e84beae9fff2697
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-03-10 17:28:16 +0900

    ✨ Jpa practice

---
+++ src/main/java/com/bongsco/poscosalarybackend/user/dto/DepartmentDto.java
@@ -0,0 +1,24 @@
+package com.bongsco.poscosalarybackend.user.dto;
+
+import com.bongsco.poscosalarybackend.user.domain.Department;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DepartmentDto {
+    private Long id;
+    private String depName;
+    private String depCode;
+
+    public DepartmentDto(Long id, String depName, String depCode) {
+        this.id = id;
+        this.depName = depName;
+        this.depCode = depCode;
+    }
+
+    public static DepartmentDto from(Department department) {
+        return new DepartmentDto(department.getId(), department.getDepName(), department.getDepCode());
+    }
+}
