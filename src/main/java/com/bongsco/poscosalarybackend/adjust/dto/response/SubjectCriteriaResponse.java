783bd8509bf862129cf1a6af983be85e71fbe093
lej8924 <eunjae8924@naver.com>
2025-04-07 11:34:09 +0900

    🎨 AdjSubjectPage PATCH API is edited

---
+++ src/main/java/com/bongsco/poscosalarybackend/adjust/dto/response/SubjectCriteriaResponse.java
@@ -0,0 +1,35 @@
+package com.bongsco.poscosalarybackend.adjust.dto.response;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class SubjectCriteriaResponse {
+
+    private LocalDate baseDate;
+    private LocalDate expStartDate;
+    private LocalDate expEndDate;
+
+    private List<SelectableItemDto> grades;
+    private List<SelectableItemDto> payments;
+
+    @Builder(toBuilder = true)
+    @AllArgsConstructor
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    @Getter
+    public static class SelectableItemDto {
+        private Long id;
+        private String name;
+        private boolean isChecked;
+    }
+}
+
