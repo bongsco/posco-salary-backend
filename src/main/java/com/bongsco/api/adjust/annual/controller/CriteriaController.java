3bccfb4ea007faefc9283c3b98f10f424e82d592
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-04-08 21:02:34 +0900

    🎨 Change return code, add swagger explanation
    g

--- src/main/java/com/bongsco/api/adjust/annual/controller/CriteriaController.java
+++ src/main/java/com/bongsco/api/adjust/annual/controller/CriteriaController.java
@@ -69,6 +69,7 @@ public class CriteriaController {
         return ResponseEntity.status(HttpStatus.CREATED).body(request);
     }
 
+    @Operation(summary = "payband 기준 설정 GET API", description = "payband 기준 가져옴")
     @GetMapping("/{adjust_id}/payband")
     public ResponseEntity<PaybandCriteriaConfigListResponse> getPaybandCriteria(
         @PathVariable(name = "adjust_id") Long adjustId) {
@@ -76,11 +77,12 @@ public class CriteriaController {
         return ResponseEntity.ok(criteriaService.getPaybandCriteria(adjustId));
     }
 
+    @Operation(summary = "payband 기준 설정 PATCH API", description = "payband 기준 설정 수정된 값 반영")
     @PatchMapping("/{adjust_id}/payband")
-    public ResponseEntity<String> updatePaybandCriteria(
+    public ResponseEntity<Void> updatePaybandCriteria(
         @Valid @RequestBody PaybandCriteriaModifyRequest request,
         @PathVariable(name = "adjust_id") Long adjustId) {
         criteriaService.updatePaybandCriteria(request);
-        return ResponseEntity.ok().build();
+        return ResponseEntity.noContent().build();
     }
 }
