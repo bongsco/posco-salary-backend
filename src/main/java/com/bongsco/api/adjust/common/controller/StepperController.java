aa1d0d6fa2d5503c2afe73cea411db71305c8c61
seoyoung0121 <88281319+seoyoung0121@users.noreply.github.com>
2025-04-09 14:36:15 +0900

    🎨 Apply review

--- src/main/java/com/bongsco/api/adjust/common/controller/StepperController.java
+++ src/main/java/com/bongsco/api/adjust/common/controller/StepperController.java
@@ -4,7 +4,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,32 +18,24 @@ import lombok.RequiredArgsConstructor;
 @Tag(name = "Stepper API", description = "Stepper 관련 API 모음")
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/stepper")
+@RequestMapping("/stepper/{adjustId}")
 public class StepperController {
     private final AdjustStepService adjustStepService;
 
     @Operation(summary = "step 불러오기", description = "stepper 정보 불러오기")
-    @GetMapping("/{adjust_id}/get-data")
-    public ResponseEntity<StepperResponse> getStepperData(@PathVariable("adjust_id") Long adjustId
+    @GetMapping
+    public ResponseEntity<StepperResponse> getStepperData(@PathVariable("adjustId") Long adjustId
     ) {
         return ResponseEntity.ok(adjustStepService.getSteps(adjustId));
     }
 
-    @Operation(summary = "step 넣기", description = "처음 조정 생성하면 stepper 관련 data db에 넣기")
-    @PostMapping("/{adjust_id}/initialize")
-    public ResponseEntity<Void> initialize(@PathVariable("adjust_id") Long adjustId
-    ) {
-        adjustStepService.initializeSteps(adjustId);
-        return ResponseEntity.noContent().build();
-    }
-
     @Operation(summary = "step 상태 바꾸기", description = "저장시 완료로 바꿀때 씀")
-    @PatchMapping("/{adjust_id}/change_state")
-    public ResponseEntity<Void> changeState(@PathVariable("adjust_id") Long adjustId,
-        @RequestParam Long AdjustStepId,
+    @PatchMapping("/step/{stepId}")
+    public ResponseEntity<Void> changeState(@PathVariable("adjustId") Long adjustId,
+        @RequestParam Long stepId,
         @RequestParam(required = false, defaultValue = "true") Boolean isDone
     ) {
-        adjustStepService.changeIsDone(AdjustStepId, isDone);
+        adjustStepService.changeIsDone(stepId, isDone);
         return ResponseEntity.noContent().build();
     }
 }
