4789406775eb588704e36b8da9041c9dfc8bb09e
Sangjin Han <49024115+eu2525@users.noreply.github.com>
2025-04-10 10:19:58 +0900

    ✨ Create adjust list API endpoints (#67)
    * :sparkles: Add GET, POST, DELETE API in Main Page
    
    * :sparkles: add transactional annotation
    
    * :sparkles: add payband criteria data save query
    
    ---------
    
    Co-authored-by: Godbell <jjongha134@gmail.com>

---
+++ src/main/java/com/bongsco/api/adjust/common/repository/StepRepository.java
@@ -0,0 +1,8 @@
+package com.bongsco.api.adjust.common.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.bongsco.api.adjust.common.entity.Step;
+
+public interface StepRepository extends JpaRepository<Step, Long> {
+}
