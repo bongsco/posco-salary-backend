a372d0d7a9993bf6aa595e48006056d1d3573330
lej8924 <eunjae8924@naver.com>
2025-04-11 09:32:06 +0900

    ➕ Add cloudfront domain

--- src/main/java/com/bongsco/api/common/config/WebConfig.java
+++ src/main/java/com/bongsco/api/common/config/WebConfig.java
@@ -10,7 +10,11 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-            .allowedOrigins("http://localhost:3000") // 프론트엔드 도메인
+            .allowedOrigins(
+                "http://localhost:3000",
+                "https://dzbukx9wf12yc.cloudfront.net",
+                "https://dupsbg9oaweks.cloudfront.net"
+            )
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 쿠키 등 인증 정보 허용
