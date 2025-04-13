51028dde319d10b5d4f377619eb724ef14045bc2
lej8924 <eunjae8924@naver.com>
2025-04-13 16:22:02 +0900

    🎨 Swagger docs & add headers

--- src/main/java/com/bongsco/api/common/utils/ExcelDownloadController.java
+++ src/main/java/com/bongsco/api/common/utils/ExcelDownloadController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,6 +24,21 @@ public class ExcelDownloadController {
     @GetMapping("/download")
     public void downloadExcel(
         @RequestParam(required = false) Long adjustId,
+        @Parameter(
+            description = """
+                엑셀 다운로드 페이지 타입 선택:
+                - 대상자 편성: 대상자(`Subject`), 비대상자(`NonSubject`)
+                - 고성과조직 대상자 목록: `highOrgSubject`
+                - Payband: 상한초과자(`upperPayband`), 하한초과자(`lowerPayband`)
+                - 정기 연봉조정 결과: `adjustResult`
+                """,
+            schema = @Schema(
+                allowableValues = {
+                    "Subject", "NonSubject", "highOrgSubject",
+                    "upperPayband", "lowerPayband", "adjustResult"
+                }
+            )
+        )
         @RequestParam String pageType,
         HttpServletResponse response
     ) throws IOException {
