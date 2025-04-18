1653954bbe1df28b6d3938ade7eb4b4395589e1c
lej8924 <eunjae8924@naver.com>
2025-04-18 09:32:59 +0900

    🎨 Change swagger: http -> https

--- web/src/main/java/com/bongsco/web/common/config/SwaggerConfig.java
+++ web/src/main/java/com/bongsco/web/common/config/SwaggerConfig.java
@@ -1,11 +1,14 @@
 package com.bongsco.web.common.config;
 
-import io.swagger.v3.oas.models.Components;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
@@ -13,7 +16,9 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
             .components(new Components())
-            .info(apiInfo());
+            .info(apiInfo())
+            .servers(List.of(
+                new Server().url("https://gbyelx4c2h.execute-api.ap-northeast-2.amazonaws.com")));
     }
 
     private Info apiInfo() {
