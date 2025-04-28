94df39538789faf61acf9b970ea8075807889ed1
Godbell <jjongha134@gmail.com>
2025-04-28 10:42:23 +0900

    🔧 Separate allowed cors origins to application yaml
    💚 Separate dev prod
    
    💚 Fix application ymls
    
    🔧 Set dev ddl create
    
    💚 Fix deploy common ymls
    
    🔥 Remove redundant code
    
    🔥 Remove redundant ymls

--- mobile/src/main/java/com/bongsco/mobile/config/WebConfig.java
+++ mobile/src/main/java/com/bongsco/mobile/config/WebConfig.java
@@ -1,22 +1,21 @@
 package com.bongsco.mobile.config;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    @Value("${cors.origins}")
+    private List<String> allowedOrigins;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-            .allowedOrigins(
-                "http://localhost:3000",
-                "http://dzbukx9wf12yc.cloudfront.net",
-                "http://dupsbg9oaweks.cloudfront.net",
-                "https://dzbukx9wf12yc.cloudfront.net",
-                "https://dupsbg9oaweks.cloudfront.net"
-            )
+            .allowedOrigins(allowedOrigins.toArray(String[]::new))
             .allowedMethods("GET", "OPTIONS") // 허용할 HTTP 메서드
             .allowedHeaders("*") // 모든 헤더 허용
             .allowCredentials(true) // 쿠키 등 인증 정보 허용
